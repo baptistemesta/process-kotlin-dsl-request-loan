a8ed3d1d12b99c0f56b018de45efd2f4c27d1539
Baptiste Mesta <baptiste.mesta@gmail.com>
2019-04-17 15:22:21 +0200

    step1: add flownodes and transitions

--- src/main/kotlin/org/bonitasoft/requestloan/RequestLoan.kt
+++ src/main/kotlin/org/bonitasoft/requestloan/RequestLoan.kt
@@ -11,6 +11,29 @@ import org.bonitasoft.engine.spring.annotations.BonitaProcess
 @BonitaProcess
 class RequestLoan : BonitaProcessBuilder {
     override fun build(): Process = process("Request Loan", "1.0") {
+        val requester = initiator("requester")
+        val validator = actor("validator")
+        contract {
+            integer named "amount"
+            text named "type"
+        }
+        val reviewRequest = userTask("Review request") {
+            actor = validator
+            contract {
+                boolean named "accept"
+                text named "reason"
+            }
+        }
+        val gateway = exclusiveGateway("accepted")
+        val signContract = userTask("Sign contract") {
+            actor = requester
+        }
+        val notifyReject = automaticTask("Notify reject")
+        transitions {
+            reviewRequest to gateway
+            gateway to signContract
+            gateway to notifyReject
+        }
     }
 
     override fun configuration(): ProcessConfiguration = configuration {
